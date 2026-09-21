235aa14dfeac839487e17658ff3f199eed8fa9f6
Anshul Jain <anshuljain2106@gmail.com>
2026-09-21 11:22:31 +0530

    js

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 // console.warn('hello');
 // console.error('hello');
 // console.table([10,202,30]);
+//alert('heyloo');
+//confirm('are you sure?')
+//prompt('emter your name ');
+
+
+// var a   //declaration
+// a = 20;
+// console.log(a); 
+
+// // data type 
+// // - premitive
+//     -Number
+//     -String
+//     -Boolean
+//     -undefined
+//     -null
+//     -BigInt
+
+// // -non premitive
+
